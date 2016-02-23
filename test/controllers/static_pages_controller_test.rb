63dd04d573ccaba790bd14016ab918cd4edfb0fd
Powerglove <lang.joh.jl@gmail.com>
2016-02-23 15:48:04 +0100

    Add html doctype and page title to views. Test titles.

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,13 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get welcome" do
     get :welcome
     assert_response :success
+    assert_select "title", "Welcome | Thinkerinnen*"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | Thinkerinnen*"
   end
 
 end

57c22ba7b8542b22ec1066f53310be1baf4c3df3
Powerglove <lang.joh.jl@gmail.com>
2016-02-23 15:36:24 +0100

    Customize Welcome and About page. Run automated tests.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,7 @@
 class StaticPagesController < ApplicationController
+  def welcome
+  end
 
-	def home
-	end
-
-	def about
-		
-	end
-	
+  def about
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  get 'static_pages/welcome'
+
+  get 'static_pages/about'
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+
+
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'

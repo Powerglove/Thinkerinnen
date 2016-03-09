c4646aeed82daede14ef9e90b01c4cf529d8e3d6
Powerglove <lang.joh.jl@gmail.com>
2016-03-09 11:54:54 +0100

    validate email format with regular expression, write tests for valid and invalid emails

--- test/models/thinker_test.rb
+++ test/models/thinker_test.rb
@@ -28,4 +28,22 @@ class ThinkerTest < ActiveSupport::TestCase
     @thinker.email = "a" * 51 + "@example.com"
     assert_not @thinker.valid?
   end
+
+  test "email validation should accept valid addresses" do
+    valid_addresses = %w[thinker@example.com THINKER@foo.COM A_US-ER@foo.bar.org
+                         first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @thinker.email = valid_address
+      assert @thinker.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[thinker@example,com thinker_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @thinker.email = invalid_address
+      assert_not @thinker.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
 end

f46f96d80800c51d3625f9d1961770566c4222fd
Powerglove <lang.joh.jl@gmail.com>
2016-03-10 12:00:35 +0100

    Add index controller test.

--- test/controllers/thinkers_controller_test.rb
+++ test/controllers/thinkers_controller_test.rb
@@ -6,6 +6,13 @@ class ThinkersControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:thinkers)
+  end
+
+
   #test "should get destroy" do
    # get :destroy
    # assert_response :success

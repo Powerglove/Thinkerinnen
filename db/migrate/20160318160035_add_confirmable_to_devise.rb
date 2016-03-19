7c9225d5bcb3d1a1dc46dab83ceb73fd57e4e5e4
Powerglove <lang.joh.jl@gmail.com>
2016-03-19 14:02:21 +0100

    rollback migration and correct it

--- db/migrate/20160318160035_add_confirmable_to_devise.rb
+++ db/migrate/20160318160035_add_confirmable_to_devise.rb
@@ -3,7 +3,7 @@ class AddConfirmableToDevise < ActiveRecord::Migration
     add_column :users, :confirmation_token, :string
     add_column :users, :confirmed_at, :datetime
     add_column :users, :confirmation_sent_at, :datetime
-    # add_column :users, :unconfirmed_email, :string # Only if using reconfirmable
+    add_column :users, :unconfirmed_email, :string # Only if using reconfirmable
     add_index :users, :confirmation_token, unique: true
     # User.reset_column_information # Need for some types of updates, but not for update_all.
     # To avoid a short time window between running the migration and updating all existing
@@ -12,12 +12,16 @@ class AddConfirmableToDevise < ActiveRecord::Migration
     # All existing user accounts should be able to log in after this.
     # Remind: Rails using SQLite as default. And SQLite has no such function :NOW.
     # Use :date('now') instead of :NOW when using SQLite.
-    execute("UPDATE users SET confirmed_at = date('now')")
+    if Rails.env.production?
+      execute("UPDATE users SET confirmed_at = NOW()")
+    else
+      execute("UPDATE users SET confirmed_at = date('now')")
+    end
     #User.all.update_all confirmed_at: Time.now
   end
 
   def down
     remove_columns :users, :confirmation_token, :confirmed_at, :confirmation_sent_at
-    # remove_columns :users, :unconfirmed_email # Only if using reconfirmable
+    remove_columns :users, :unconfirmed_email # Only if using reconfirmable
   end
 end

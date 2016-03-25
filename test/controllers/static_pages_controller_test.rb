d282a323c5fbb17672f73553807a36050c4279ac
Powerglove <lang.joh.jl@gmail.com>
2016-03-25 13:12:16 +0100

    Fix static pages controller tests, delete fixtures that made test fail

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,14 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  include Devise::TestHelpers
+
+  def setup
+    @request.env["devise.mapping"] = Devise.mappings[:admin]
+    sign_in User.create(email: "thinker@example.com", password: "password", confirmed_at: 1.day.ago)
+  end
+  
+
   test "should get welcome" do
     get :welcome
     assert_response :success

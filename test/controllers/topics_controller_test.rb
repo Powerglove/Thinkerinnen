907d7d4a60cb8760a87bb4f6ddc9296db620dfc5
Powerglove <lang.joh.jl@gmail.com>
2016-04-06 10:50:35 +0200

    fix test

--- test/controllers/topics_controller_test.rb
+++ test/controllers/topics_controller_test.rb
@@ -1,9 +1,22 @@
 require 'test_helper'
 
 class TopicsControllerTest < ActionController::TestCase
+	include Devise::TestHelpers
+
+  #test "should get index" do
+   # get :index
+    #assert_response :success
+  #end
+
   test "should get index" do
-    get :index
-    assert_response :success
-  end
+  	thinker = Thinker.create(name: "Test Thinker", email: "test@example.com")
+  	thinker.topic_list = "logic, math"
+  	thinker.save
+
+  	get :index, topic: 'logic'
+
+  	assert_equal [thinker], assigns(:thinkers)
+
+	end
 
 end

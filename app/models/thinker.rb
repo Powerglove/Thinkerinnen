23f07551e5c29b17b7fe484ec415252349fe229b
Powerglove <lang.joh.jl@gmail.com>
2016-05-10 20:42:50 +0200

    add allow blank to email

--- app/models/thinker.rb
+++ app/models/thinker.rb
@@ -2,7 +2,7 @@ class Thinker < ActiveRecord::Base
 	validates :name, presence: true, length: { maximum: 50}
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, length: { maximum: 50},
-	          format: { with: VALID_EMAIL_REGEX }
+	          format: { with: VALID_EMAIL_REGEX }, allow_blank: true
 	acts_as_taggable_on :topics
 	has_many :references
 end
